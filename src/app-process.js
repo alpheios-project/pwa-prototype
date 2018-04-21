9166ae8ce36b2f49241ebeeb72063de4f29623d8
Kirlat <kirlat@yula.media>
2018-04-21 16:23:18 +0400

    Converted default settings to JSON. Added defaults loader class. Created a storage adapter class

--- src/app-process.js
+++ src/app-process.js
@@ -1,7 +1,7 @@
 import {Constants} from 'alpheios-data-models'
 import {AlpheiosTuftsAdapter} from 'alpheios-morph-client'
 import {Lexicons} from 'alpheios-lexicon-client'
-import { HTMLSelector, LexicalQuery, ContentOptionDefaults, LanguageOptionDefaults, Options,
+import { HTMLSelector, LexicalQuery, ContentOptionDefaults, LanguageOptionDefaults, DefaultsLoader, Options,
   LocalStorageArea, UIController, UIStateAPI, PopupMod } from 'alpheios-components'
 
 export default class AppProcess {
@@ -9,19 +9,9 @@ export default class AppProcess {
     this.state = new UIStateAPI()
     this.state.status = UIStateAPI.statuses.script.PENDING
     this.state.panelStatus = UIStateAPI.statuses.panel.CLOSED
-    const contentOptionsDefaults = new ContentOptionDefaults()
-    this.options = new Options(
-      contentOptionsDefaults,
-      LocalStorageArea.get.bind(this, contentOptionsDefaults.storageDomain),
-      LocalStorageArea.set.bind(this, contentOptionsDefaults.storageDomain)
-    )
+    this.options = new Options(DefaultsLoader.getJSON(ContentOptionDefaults), LocalStorageArea)
     this.maAdapter = new AlpheiosTuftsAdapter() // Morphological analyzer adapter, with default arguments
-    const languageOptionsDefaults = new LanguageOptionDefaults()
-    this.langOptions = new Options(
-      languageOptionsDefaults,
-      LocalStorageArea.get.bind(this, languageOptionsDefaults.storageDomain),
-      LocalStorageArea.set.bind(this, languageOptionsDefaults.storageDomain)
-    )
+    this.langOptions = new Options(DefaultsLoader.getJSON(LanguageOptionDefaults), LocalStorageArea)
     this.ui = new UIController(this.state, this.options, this.langOptions, {}, { popupComponent: PopupMod })
     document.body.addEventListener('dblclick', this.getSelectedText.bind(this))
   }
