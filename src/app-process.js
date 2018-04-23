ddd29c706e5c0fdf26378068a5567202b91fa06a
Kirlat <kirlat@yula.media>
2018-04-23 14:52:03 +0400

    Added a skin switcher

--- src/app-process.js
+++ src/app-process.js
@@ -1,18 +1,23 @@
 import {Constants} from 'alpheios-data-models'
 import {AlpheiosTuftsAdapter} from 'alpheios-morph-client'
 import {Lexicons} from 'alpheios-lexicon-client'
-import { HTMLSelector, LexicalQuery, ContentOptionDefaults, LanguageOptionDefaults, DefaultsLoader, Options,
-  LocalStorageArea, UIController, UIStateAPI, PopupMod } from 'alpheios-components'
+import { HTMLSelector, LexicalQuery, ContentOptionDefaults, LanguageOptionDefaults, UIOptionDefaults, DefaultsLoader,
+  Options, LocalStorageArea, UIController, UIStateAPI, PopupMod } from 'alpheios-components'
+// CSS skins
+import SkinBlue from '../node_modules/alpheios-components/dist/skins/blue/style.min.css'
+import SkinGreen from '../node_modules/alpheios-components/dist/skins/green/style.min.css'
 
 export default class AppProcess {
   constructor () {
     this.state = new UIStateAPI()
     this.state.status = UIStateAPI.statuses.script.PENDING
     this.state.panelStatus = UIStateAPI.statuses.panel.CLOSED
-    this.options = new Options(DefaultsLoader.getJSON(ContentOptionDefaults), LocalStorageArea)
+    this.options = new Options(DefaultsLoader.fromJSON(ContentOptionDefaults), LocalStorageArea)
     this.maAdapter = new AlpheiosTuftsAdapter() // Morphological analyzer adapter, with default arguments
-    this.langOptions = new Options(DefaultsLoader.getJSON(LanguageOptionDefaults), LocalStorageArea)
-    this.ui = new UIController(this.state, this.options, this.langOptions, {}, { popupComponent: PopupMod })
+    this.langOptions = new Options(DefaultsLoader.fromJSON(LanguageOptionDefaults), LocalStorageArea)
+    this.uiOptions = new Options(DefaultsLoader.fromJSON(UIOptionDefaults), LocalStorageArea)
+    this.uiOptions.items.skin.addValue('blue', 'Blue Skin').addValue('green', 'Green Skin')
+    this.ui = new UIController(this.state, this.options, this.langOptions, this.uiOptions, {}, { popupComponent: PopupMod })
     document.body.addEventListener('dblclick', this.getSelectedText.bind(this))
   }
 
--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 import ComponentStyles from '../node_modules/alpheios-components/dist/style/style.min.css'
-import SkinBlue from '../node_modules/alpheios-components/dist/skins/blue/style.min.css'
-import SkinGreen from '../node_modules/alpheios-components/dist/skins/green/style.min.css'
 import Css from './style/style.scss'
 import AppProcess from './app-process.js'
 
